bd58be9148134e55b2e63868c7c031aef0684079
NovitskiyAleksei <novitskiy.aleksei@gmail.com>
2016-03-06 15:22:02 +0200

    opencart init

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:9999/admin/');
+define('HTTP_CATALOG', 'http://localhost:9999/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:9999/admin/');
+define('HTTPS_CATALOG', 'http://localhost:9999/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/novik/Documents/projects/glory_shop/upload/admin/');
+define('DIR_SYSTEM', '/Users/novik/Documents/projects/glory_shop/upload/system/');
+define('DIR_IMAGE', '/Users/novik/Documents/projects/glory_shop/upload/image/');
+define('DIR_LANGUAGE', '/Users/novik/Documents/projects/glory_shop/upload/admin/language/');
+define('DIR_TEMPLATE', '/Users/novik/Documents/projects/glory_shop/upload/admin/view/template/');
+define('DIR_CONFIG', '/Users/novik/Documents/projects/glory_shop/upload/system/config/');
+define('DIR_CACHE', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/upload/');
+define('DIR_CATALOG', '/Users/novik/Documents/projects/glory_shop/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'gloryshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:9999/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:9999/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/novik/Documents/projects/glory_shop/upload/catalog/');
+define('DIR_SYSTEM', '/Users/novik/Documents/projects/glory_shop/upload/system/');
+define('DIR_IMAGE', '/Users/novik/Documents/projects/glory_shop/upload/image/');
+define('DIR_LANGUAGE', '/Users/novik/Documents/projects/glory_shop/upload/catalog/language/');
+define('DIR_TEMPLATE', '/Users/novik/Documents/projects/glory_shop/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/novik/Documents/projects/glory_shop/upload/system/config/');
+define('DIR_CACHE', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/download/');
+define('DIR_LOGS', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/novik/Documents/projects/glory_shop/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'gloryshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
